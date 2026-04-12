95e99e31aa15843e94f431684f2f75ad983baeee
Allen Guo <guosonglin001@gmail.com>
2026-04-12 23:05:43 +0800

    revert

--- tinygrad/runtime/support/am/ip.py
+++ tinygrad/runtime/support/am/ip.py
@@ -1,15 +1,16 @@
-import ctypes, time
-from typing import Literal, TYPE_CHECKING
-from tinygrad.runtime.autogen.am import am, smu_v13_0_0
-from tinygrad.helpers import to_mv, data64, lo32, hi32, DEBUG
-
-if TYPE_CHECKING:
-  from tinygrad.runtime.support.am.amdev import AMDev
+import ctypes, time, contextlib, functools
+from typing import cast, Literal
+from tinygrad.helpers import to_mv, data64, lo32, hi32, DEBUG, wait_cond, pad_bytes, getbits
+from tinygrad.runtime.autogen.am import am
+from tinygrad.runtime.support.amd import import_soc
+from tinygrad.runtime.support.memory import AddrSpace
 
 class AM_IP:
-  def __init__(self, adev: 'AMDev'): self.adev = adev
-  def init(self): raise NotImplementedError("IP block init must be implemeted")
-  def fini(self): pass
+  def __init__(self, adev): self.adev = adev
+  def init_sw(self): pass # Prepare sw/allocations for this IP
+  def init_hw(self): pass # Initialize hw for this IP
+  def fini_hw(self): pass # Finalize hw for this IP
+  def set_clockgating_state(self): pass # Set clockgating state for this IP
 
 class AM_SOC(AM_IP):
   def init_sw(self):
